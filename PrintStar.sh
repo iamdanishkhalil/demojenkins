7846cda84dfaebd310f2dffaa4d1a3970d40f13a
Mohammad Danish Khalil <143552441+iamdanishkhalil@users.noreply.github.com>
2024-03-18 02:31:58 +0530

    Update PrintStar.sh

--- PrintStar.sh
+++ PrintStar.sh
@@ -26,6 +26,5 @@ read -p "Enter the number of rows: " rows
 
 # Call function to print triangle pattern
 print_triangle $rows
-```
 
 This script prompts the user to enter the number of rows for the triangle and then prints the triangle star pattern accordingly.
